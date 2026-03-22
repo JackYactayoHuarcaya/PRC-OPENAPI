ec2f586e0d23fd8efae3cafc6d577794f5190386
jack yactayo hurcaya <jack-0204@hotmail.com>
2026-03-21 23:57:48 -0500

    feat: prc - openapi

---
+++ src/test/java/com/prcopenapi/prcopenapi/PrcOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prcopenapi.prcopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrcOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
